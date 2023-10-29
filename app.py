01adc813366c20ba3f9eb3627fa3c964ea9d29e8
NalinHong <66056044@kmitl.ac.th>
2023-10-29 11:29:56 +0700

    commit tree graph streamlit

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# --- 01
-# https://docs.streamlit.io/library/api-reference/write-magic
-st.markdown('สวัสดี! **Streamlit**')
-st.write('จากโค้ด', '`st.markdown("สวัสดี!")`')
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
-}))
-st.divider()
+#
+# # --- 01
+# # https://docs.streamlit.io/library/api-reference/write-magic
+# st.markdown('สวัสดี! **Streamlit**')
+# st.write('จากโค้ด', '`st.markdown("สวัสดี!")`')
+# st.write(pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40],
+# }))
+# st.divider()
 
 # --- 02
 # binom_dist = np.random.binomial(1, .5, 100)
@@ -59,3 +59,27 @@ st.divider()
 # # 2. how to setup limit of x-axis value to 0.0 - 1.0?
 # # 3. how to setup more bins, like 20 or 40?
 # st.pyplot(fig1)
+# all#=ctrl+/
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.markdown('สวัสดี! **Streamlit**')
+st.title('Layout and decoration')
+st.write('''ลองทำSan Francisco Dataset กัน''')
+col1, col2, col3 =st.columns(3)
+with col1:
+    st.write('Column 1')
+with col2:
+    st.write('Column 2')
+with col3:
+    st.write('Column 3')
+trees_df = pd.read_csv('trees.csv')
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns=['tree_count']
+st.line_chart(df_dbh_grouped)
+st.caption('กราฟ แสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่านศนย์กลาง')
+st.title('แปลผล')
+st.write('''ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3''')
