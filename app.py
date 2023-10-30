4ba31bd934aa3252255fb9979082e9181d255324
NalinHong <66056044@kmitl.ac.th>
2023-10-30 10:32:28 +0700

    app.py commit

--- app.py
+++ app.py
@@ -69,13 +69,19 @@ import matplotlib.pyplot as plt
 st.markdown('สวัสดี! **Streamlit**')
 st.title('Layout and decoration')
 st.write('''ลองทำSan Francisco Dataset กัน''')
+
+trees_df = pd.read_csv('trees.csv')
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns=['tree_count']
+
 col1, col2, col3 =st.columns(3)
 with col1:
-    st.write('Column 1')
+    st.line_chart(df_dbh_grouped)
 with col2:
-    st.write('Column 2')
+    st.bar_chart(df_dbh_grouped)
 with col3:
-    st.write('Column 3')
+    st.area_chart(df_dbh_grouped)
+
 trees_df = pd.read_csv('trees.csv')
 df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns=['tree_count']
@@ -83,3 +89,7 @@ st.line_chart(df_dbh_grouped)
 st.caption('กราฟ แสดงจำนวนต้นไม้ จัดกลุ่มตามเส้นผ่านศนย์กลาง')
 st.title('แปลผล')
 st.write('''ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3''')
+
+trees_df =trees_df.dropna(subset=['longitude','latitude'])
+trees_df= trees_df.sample(n=1000,replace=True)
+st.map(trees_df)
